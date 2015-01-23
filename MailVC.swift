ea32f0c6b56fdaac6cf13131036f0a8ffcd4710a
Faisal Maqsood Qadri <FaisalHome@Faisal-Mac.home>
2015-01-23 00:38:40 +0000

    New File Added for mail

---
+++ MailVC.swift
@@ -0,0 +1,14 @@
+//
+//  MailVC.swift
+//  FaisalApp
+//
+//  Created by Faisal Maqsood Qadri on 23/01/2015.
+//  Copyright (c) 2015 Bluekit Solutions. All rights reserved.
+//
+
+import Foundation
+
+class MailVC {
+    
+    
+}
