f438184cd5d333d51f13cc02f4f5e98f12f445b2
Faisal Maqsood Qadri <FaisalHome@Faisal-Mac.home>
2015-01-23 00:02:37 +0000

    Initial Commit

---
+++ FaisalApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaisalApp
+//
+//  Created by Faisal Maqsood Qadri on 23/01/2015.
+//  Copyright (c) 2015 Bluekit Solutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
